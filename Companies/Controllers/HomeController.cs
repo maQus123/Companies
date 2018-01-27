caf74082d3cd47fe4c49b3fd14ef958be7a03d84
Markus <markus@lmaen.de>
2018-01-27 20:48:57 +0100

    init

--- Companies/Controllers/HomeController.cs
+++ Companies/Controllers/HomeController.cs
@@ -1,37 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Companies.Models;
+﻿namespace Companies.Controllers {
 
-namespace Companies.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : Controller {
+
+        public IActionResult Index() {
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
+
 }
---
+++ Companies/Models/Company.cs
@@ -0,0 +1,21 @@
+﻿namespace Companies.Models {
+
+    public class Company {
+
+        public int Id { get; private set; }
+
+        public int AmountEmployees { get; set; }
+
+        public string Branch { get; set; }
+
+        public string Title { get; set; }
+
+        public string Town { get; set; }
+
+        public Company() {
+            //nothing to do
+        }
+
+    }
+
+}
